456a2124f9919ff7b347e28edcb5df1f232e298d
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2022-05-18 03:28:10 +0000

    :sparkles: Builds functions to generate HTMLElements

--- src/app.js
+++ src/app.js
@@ -17,7 +17,65 @@ window.onload = function() {
 
 function render() {
   const app = document.querySelector(".app");
-  const p = document.createElement("p");
-  p.textContent = "working on this";
-  app.appendChild(p); // window.currentCard.asHTML()
+
+  // create a card element
+  const card = buildCard();
+
+  // create a top-suit element
+  const topSuit = buildSuit("top");
+
+  // create a bottom-suit element
+  const bottomSuit = buildSuit("bottom");
+
+  // create a face-value element
+  const faceValue = buildFaceValue();
+
+  // append all children
+  card.append(topSuit, faceValue, bottomSuit);
+
+  // replace app's content with this elements
+  app.innerHTML = "";
+  app.appendChild(card);
+}
+
+function buildCard() {
+  // creates div and adds class names and styles
+  const card = document.createElement("div");
+  card.className = "d-flex flex-column justify-content-between bg-white";
+  card.style.width = "20rem";
+  card.style.height = "30rem";
+  card.style.border = "solid black 2px";
+  card.style.borderRadius = "1rem";
+  return card;
+}
+
+function buildSuit() {
+  // creates div and adds common suit styles and values
+  const suit = document.createElement("div");
+  suit.style.fontSize = "5rem";
+  suit.style.color = window.currentCard.suit.color;
+  suit.textContent = window.currentCard.suit.value;
+  // using JS arguments variable for functions...
+  for (let arg of Object.values(arguments)) {
+    // we add class names based on 'top' or 'bottom' argument
+    if (arg === "top") {
+      suit.className = "d-flex ms-4 align-self-start";
+    }
+    if (arg === "bottom") {
+      suit.className = "d-flex me-4 align-self-end";
+      suit.style.transform = "rotate(180deg)";
+    }
+  }
+  return suit;
+}
+
+function buildFaceValue() {
+  // create div and add values and classnames
+  const value = document.createElement("div");
+  value.className = "d-flex align-self-center";
+  value.style.fontWeight = "600";
+  value.style.fontSize = "5rem";
+  value.style.color = window.currentCard.suit.color;
+  value.textContent = window.currentCard.value;
+  return value;
 }
